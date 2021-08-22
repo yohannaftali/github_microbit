c1bb3105145cf3cf9ddb08efbd2943e8ab31c3aa
Yohan Naftali <yohan.naftali@gmail.com>
2021-08-22 15:06:00 +0700

    melody

--- main.ts
+++ main.ts
@@ -11,4 +11,5 @@ basic.forever(function () {
     music.playMelody("C - C - G - G - ", 210)
     music.playMelody("A - A - G G G - ", 210)
     music.playMelody("F - F - E - E - ", 210)
+    music.playMelody("D - D - C C C - ", 210)
 })
