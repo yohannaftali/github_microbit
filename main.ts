c260667e6e00e80d928f45240f0c487c5726e525
Yohan Naftali <yohan.naftali@gmail.com>
2021-08-22 15:05:04 +0700

    play melody

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+music.onEvent(MusicEvent.MelodyNotePlayed, function () {
+    basic.showLeds(`
+        . . # # .
+        . # . # .
+        # . . # .
+        # . . # #
+        # # . . .
+        `)
+})
 basic.forever(function () {
-	
+    music.playMelody("C - C - G - G - ", 210)
+    music.playMelody("A - A - G G G - ", 210)
+    music.playMelody("F - F - E - E - ", 210)
 })
