2a33acbce9a8258d6521929bf4d1b1d859aeea1d
Yohan Naftali <yohan.naftali@gmail.com>
2021-08-22 15:14:19 +0700

    led

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onButtonPressed(Button.A, function () {
+    music.stopMelody(MelodyStopOptions.Foreground)
+})
+music.onEvent(MusicEvent.MelodyEnded, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+})
 music.onEvent(MusicEvent.MelodyNotePlayed, function () {
     basic.showLeds(`
         . . # # #
@@ -7,9 +22,10 @@ music.onEvent(MusicEvent.MelodyNotePlayed, function () {
         # # . . .
         `)
 })
+music.playMelody("C - C - G - G - ", 210)
+music.playMelody("A - A - G G G - ", 210)
+music.playMelody("F - F - E - E - ", 210)
+music.playMelody("D - D - C C C - ", 210)
 basic.forever(function () {
-    music.playMelody("C - C - G - G - ", 210)
-    music.playMelody("A - A - G G G - ", 210)
-    music.playMelody("F - F - E - E - ", 210)
-    music.playMelody("D - D - C C C - ", 210)
+	
 })
